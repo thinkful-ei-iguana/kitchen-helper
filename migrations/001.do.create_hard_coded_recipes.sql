803d9a4f523b6e0a318e4f7aecc9e464817d2282
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-12-31 10:59:27 -0700

    working migrations and seeding to database

--- migrations/001.do.create_hard_coded_recipes.sql
+++ migrations/001.do.create_hard_coded_recipes.sql
@@ -1,12 +1,23 @@
+DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS recipes;
-
-
 
 
 CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
-  user_name TEXT NOT NULL UNIQUE,
+  title TEXT NOT NULL UNIQUE,
   recipe_description TEXT NOT NULL,
-  recipe_ingredients TEXT NOT NULL UNIQUE,
+  recipe_ingredients TEXT NOT NULL,
+  time_to_make TEXT NOT NULL,
+  date_created TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+
+
+CREATE TABLE accounts (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  user_name TEXT NOT NULL UNIQUE,
+  user_email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
   date_created TIMESTAMPTZ NOT NULL DEFAULT now()
 );
--- seeds/seed.recipes.sql
+++ seeds/seed.recipes.sql
@@ -2,21 +2,23 @@ BEGIN;
 
 TRUNCATE
   recipes,
-  users
+  accounts
   RESTART IDENTITY CASCADE;
-psql -U dunder_mifflin -d recipes_test -f ./seeds/seed/thingful_tables.sql
 
-INSERT INTO users (first_name, last_name, email, password)
+-- psql -U dunder_mifflin -d kitchen-helper -f C:\Users\calvi\OneDrive\Documents\GitHub\kitchen-helper\seeds\seed.recipes.sql
+
+
+INSERT INTO accounts (first_name, user_name, user_email, password)
 VALUES
-  ('Calvin', 'Rosehart', 'testuser.com', 'abcde12345'),
-  ('Mandee', 'Rosehart', 'recipemaker@yahoo.com', 'abcd1234');
+  ('Calvin', 'Thunder', 'testuser.com', 'abcde12345'),
+  ('Mandee', 'Lightning', 'recipemaker@yahoo.com', 'abcd1234');
 
 
-INSERT INTO recipes (title, ingredients, recipe_description, time_to_make)
+INSERT INTO recipes (title, recipe_description, recipe_ingredients, time_to_make)
 VALUES
-  ('stew', 'meat, potatoes, beef stock, spices', 'makes meat and potato stew for 4', 24:30),
-  ('bread', 'flour, yeast, sugar, milk, etc', 'makes a loaf of tasty homemade bread', 35:00);
-  ('meat_pie', 'ground beef, sugar, flour, corn, chopped onion, water, etc', 'makes a meat filled pie with upper and low crust, feeds 6', 25:00);
+  ('Stew', 'makes meat and potato stew for 4', 'meat, potatoes, beef stock, spices', '24:30'),
+  ('Bread', 'makes a loaf of tasty homemade bread', 'flour, yeast, sugar, milk, etc', '35:00'),
+  ('Meat-Pie', 'makes a meat filled pie with upper and low crust, feeds 6', 'ground beef, sugar, flour, corn, chopped onion, water, etc', '25:00');
   
 
 
