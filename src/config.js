3b16505c80ca9a960eaadf3e424d7ee086927994
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-01-10 10:09:40 -0700

    testing with anthony V2

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/kitchen-helper",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/kitchen-helper-test",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
 };
