b72247043f011bbeef5bb913f74161cf510ba91c
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-01-10 10:25:07 -0700

    anthony test V5

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
 const knex = require("knex");
 
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
+
 const db = knex({
   client: "pg",
   connection: DATABASE_URL
 });
 
 app.set("db", db);
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
